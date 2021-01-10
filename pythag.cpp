a7d97654a187500b88a38f965fa85b9cbd4427c0
Sonny <48072766+SonnyIsMario@users.noreply.github.com>
2021-01-09 22:14:39 -0500

    Update pythag.cpp

--- pythag.cpp
+++ pythag.cpp
@@ -1,7 +1,6 @@
 #include "english.h"
 #include <cmath>
-
-int main () {
+Interger Foremost Left_Parenthesis Right_Parenthesis Left_Bracket
 float a;
 float b;
 float c;
@@ -13,5 +12,5 @@ std::cin >> b;
 c = sqrt(pow(a,2) + pow(b,2));
 std::cout << c;
 return 0;
-}
+Right_Bracket
 //c = sqrt(pow(a,2) + pow(b,2));
