9c5a65271a7d006862b39bb13ecf0b91830ec73e
Sonny <48072766+SonnyIsMario@users.noreply.github.com>
2021-01-09 22:17:04 -0500

    Update pythag.cpp

--- pythag.cpp
+++ pythag.cpp
@@ -1,9 +1,9 @@
 #include "english.h"
 #include <cmath>
 Interger Foremost Left_Parenthesis Right_Parenthesis Left_Bracket
-float a;
-float b;
-float c;
+Double a;
+Double b;
+Double c;
 
 std::cout << "enter a leg: ";
 std::cin >> a;
@@ -11,6 +11,6 @@ std::cout << "enter leg two: ";
 std::cin >> b;
 c = sqrt(pow(a,2) + pow(b,2));
 std::cout << c;
-return 0;
+Bring_Back 0;
 Right_Bracket
 //c = sqrt(pow(a,2) + pow(b,2));
